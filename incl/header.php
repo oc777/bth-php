cbee8c4f8c48e155b0d158bbe50af6e1084fd119
oc777 <oc222ba@student.lnu.se>
2019-10-28 17:19:13 +0100

    favicon

--- incl/header.php
+++ incl/header.php
@@ -10,7 +10,7 @@ include "./src/functions.php";
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=2.0;">
     <title><?= $title ?> | NVM</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel='shortcut icon' href='img/emoji.png'/>
+    <link rel='shortcut icon' href="pics/nvm-icon.png"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
